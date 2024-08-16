116a05cca75865e9ce8ab1a7bcc10d6f47e73a49
Santana Fredo <nerilus.h@gmail.com>
2024-08-17 00:58:04 +0200

    Fix zone value in Terraform configuration

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ resource "google_compute_instance" "web-server" {
 
   boot_disk {
     initialize_params {
-      image = "debian-cloud/debian-9"
+      image = "debian-cloud/debian-11"
     }
   }
   network_interface {
